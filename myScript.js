e3a5cc7482c13d37c7575877766cea04e4237196
Olumide <olumideawofeso@gmail.com>
2015-02-07 19:18:52 -0500

    Fixed a nasty bug and 'refactored'

--- myScript.js
+++ myScript.js
@@ -1,98 +1,120 @@
-function gup(name){
-hashornot = window.location.search;
-if(document.URL.indexOf("#")!==-1){hashornot=window.location.hash.replace("#","#hold=1&");}
-name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-var regexS = "[\\?&]"+name+"=([^&#]*)";
-var regex = new RegExp( regexS );
-var results = regex.exec(hashornot);
-if( results == null )
-  return "";
-else
-  return results[1];
+function getParameter(name){
+	hashornot = window.location.search;
+	if(document.URL.indexOf("#")!==-1){hashornot=window.location.hash.replace("#","#hold=1&");}
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec(hashornot);
+	if( results == null )
+		return "";
+	else
+		return results[1];
 }
 
-function whichsite() {
-sEngine=0;
-if (document.URL.indexOf(".google.")!==-1){sEngine="google";}
-else if (document.URL.indexOf(".bing.com")!==-1){sEngine="bing";}
-return(sEngine)
+function whichSite() {
+	sEngine = null;
+	if (location.hostname.indexOf(".google.")!==-1){
+		sEngine = "google";
+	}
+	else if (location.hostname.indexOf(".bing.com")!==-1){
+		sEngine = "bing";
+	}
+	return(sEngine);
 }
 
 function issearchorimage(){
-site=whichsite();
-if(((document.URL.indexOf("search") !== -1)||(document.URL.indexOf("/#") !== -1)||(document.URL.indexOf("#newwindow") !== -1)) && (gup("q")!==""))
-{
-  if ((site=="bing")&&(document.URL.indexOf("/images/")!==-1)){s_type = "bingimage"}
-  else if ((site=="bing")&&(document.URL.indexOf(".com/search")!==-1)){s_type = "bingweb"}
-  else {s_type=0;}
-  if ((site=="google")&&(gup("tbm")=="isch")){s_type = "googleimage"}
-  else if ((site=="google")&&(gup("tbm")=="")){s_type = "googleweb"}
-  else if ((site=="google")&&(gup("tbm")!=="")){s_type = 0}
-}
-else {s_type=0;}
-return(s_type)
+	searchType = null;
+	site = whichSite();
+	if(site !== null){
+		if(((document.URL.indexOf("/search") !== -1)||(document.URL.indexOf("/#") !== -1)||(document.URL.indexOf("#newwindow") !== -1)) && (getParameter("q")!=="")){
+			if ((site == "bing")&&(document.URL.indexOf("/images/")!==-1)){
+				searchType = "image"
+			}
+			else if ((site=="bing")&&(document.URL.indexOf(".com/search")!==-1)){
+				searchType = "web";
+			}
+			else if ((site=="google")&&(getParameter("tbm")=="isch")){
+				searchType = "image";
+			}
+			else if ((site=="google")&&(getParameter("tbm")=="")){
+				searchType = "web";
+			}
+		}
+	}
+	return([site, searchType]);
 }
 
 function start_script(){
-  data = issearchorimage();
-  if (data !== 0){
-    site_name = whichsite();
-    if(site_name == "google"){site_name2 = "bing";}
-    else {site_name2 = "google";}
-    if (site_name=="google"){img_link = chrome.extension.getURL("g2b.png");}
-    else if (site_name=="bing"){img_link = chrome.extension.getURL("b2g.png");}
-    //var localestuff = whatcc();
-    if(data.indexOf("web")!==-1){link = "http://www."+site_name2+".com/search?q="+gup("q");}
-    else if(data =="googleimage"){link = "http://www."+site_name2+".com/images/search?q="+gup("q");}
-    else if(data == "bingimage"){link = "http://www."+site_name2+".com/search?q="+gup("q")+"&tbm=isch";}
-    return(link);
-  }else{return(0);}
+	link = null;
+	data = issearchorimage();
+	destDomain = "bing";
+	if (data[1] !== null){
+		site_name = data[0];
+		searchType = data[1];
+		if (site_name == "google"){
+			img_link = chrome.extension.getURL("g2b.png");
+		}
+		else if (site_name=="bing"){
+			img_link = chrome.extension.getURL("b2g.png");
+			destDomain = "google";
+		}
+		if(searchType == "web"){
+			link = "//www."+destDomain+".com/search?q="+getParameter("q");
+		}
+		else if(searchType == "image"){
+			if(site_name == "google"){
+				link = "//www.bing.com/images/search?q="+getParameter("q");
+			}
+			else if(site_name == "bing"){
+				link = "//www.google.com/search?q="+getParameter("q")+"&tbm=isch";
+			}
+		}
+	}
+	if(link != null){
+		if(!document.getElementById("bingtogoogle")){
+			create_button(data[0], link);
+		}
+		else{
+			updateLink(link);
+		}
+	}
+	return(link);
 }
 
-function create_button(){
-  site_name = whichsite();
-  if(site_name == "google"){site_name2 = "bing";}
-  else {site_name2 = "google";}
-  if (site_name=="google"){img_link = chrome.extension.getURL("g2b.png");}
-  else if (site_name=="bing"){img_link = chrome.extension.getURL("b2g.png");}
-  var mydiv = document.createElement('div');
-  mydiv.setAttribute('style', 'background-image:url("'+img_link+'");'+'-webkit-box-shadow: 0px -1px 7px rgba(50, 50, 50, 0.75); box-shadow: 0px -1px 7px rgba(50, 50, 50, 0.75); background-size:100%; width:45px; height:45px; position:fixed; right:2px; bottom:2px; z-index:999999999; cursor:pointer;');
-  mydiv.setAttribute('id', 'bingtogoogle');
-  var mylink = document.createElement('a');
-  mylink.appendChild(mydiv);
-  mylink.setAttribute("id", "clickme");
-  document.body.appendChild(mylink);
-  var clickedme = document.getElementById("clickme");
-  clickedme.addEventListener("click", function() {
-    //alert(link2go);
-    location.href=link2go;
-  }, false);
+function create_button(site_name, link){
+	if(site_name=="google"){
+		img_link = chrome.extension.getURL("g2b.png");
+	}
+	else if (site_name=="bing"){
+		img_link = chrome.extension.getURL("b2g.png");
+	}
+	var mydiv = document.createElement('div');
+	mydiv.setAttribute('style', 'background-image:url("'+img_link+'");'+ cssProp);
+	mydiv.setAttribute('id', 'bingtogoogle');
+	var mylink = document.createElement('a');
+	mylink.appendChild(mydiv);
+	mylink.setAttribute("id", "clickme");
+	mylink.setAttribute("href", link);
+	document.body.appendChild(mylink);
+/*
+	var clickedme = document.getElementById("clickme");
+	clickedme.addEventListener("click", 
+		function() {
+			location.href=linkDest;
+		}, false);
+*/
 }
 
-function whatcc()
-{
-//cookie stuff for bing
-//remmeber language and locale are different things
-
+function updateLink(link){
+	var clickme = document.getElementById("clickme");
+	clickme.setAttribute("href", link);
 }
 
-var link2go = start_script();
-if (link2go != 0){
-  if (document.getElementById("bingtogoogle")){
-    var de = "fre";
-  }else{
-    create_button();
-  }
-}
+var cssProp = '-webkit-box-shadow: 0px -1px 7px rgba(50, 50, 50, 0.75); box-shadow: 0px -1px 7px rgba(50, 50, 50, 0.75); background-size:100%; width:45px; height:45px; position:fixed; right:2px; bottom:2px; z-index:999999999; cursor:pointer;';
+var linkDest = start_script();
 
 
-window.onhashchange = function(){
-  link2go = start_script();
-  if (link2go != 0){
-    if (document.getElementById("bingtogoogle")){
-      var de = "fre";
-    }else{
-      create_button();
-    }
-  }
+window.onhashchange = function(e){
+    e = e || window.event;
+    linkDest = start_script();
 }
