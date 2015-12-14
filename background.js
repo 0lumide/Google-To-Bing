3acc6417c92d4b43ced8f45718ae0c31bfb01694
Olumide Awofeso <olumideawofeso@gmail.com>
2015-12-14 09:31:19 -0500

    Same bug fix but for reverse direction

--- background.js
+++ background.js
@@ -2,3 +2,9 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log('Page uses History API and we heard a pushSate/replaceState.');
     chrome.tabs.sendMessage(details.tabId, {"event": "historyChange"});
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    console.log('Tab updated');
+    chrome.tabs.sendMessage(tabId, {"event": "tab Updated"});
+});
+
